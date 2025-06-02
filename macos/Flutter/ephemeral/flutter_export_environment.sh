7fb6a1a61e11f256b7407b92261069ee9d99faa6
Mohamed Elkerm Laptop <mohamedelkerm584@gmail.com>
2025-06-02 23:26:57 +0300

    .

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/dinaelhallaq/Documents/developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/dinaelhallaq/Documents/GitHub/Jetak-mobile-client-develop"
+export "FLUTTER_ROOT=C:\Users\ElKerm\fvm\default"
+export "FLUTTER_APPLICATION_PATH=D:\jetak\Jetak-mobile-client-develop"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=2.7.0"
