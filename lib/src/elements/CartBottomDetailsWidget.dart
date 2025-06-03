94a84722273582cda982e3f45d16c71aeaecc7a9
Mohamed Elkerm Laptop <mohamedelkerm584@gmail.com>
2025-06-03 14:18:35 +0300

    the payment complete

--- lib/src/elements/CartBottomDetailsWidget.dart
+++ lib/src/elements/CartBottomDetailsWidget.dart
@@ -5,6 +5,8 @@ import '../controllers/cart_controller.dart';
 import '../helpers/app_colors.dart';
 import '../helpers/app_text_styles.dart';
 import '../helpers/helper.dart';
+import '../models/route_argument.dart';
+import '../pages/order_success.dart';
 import 'custom_material_button.dart' show CustomMaterialButton;
 
 class CartBottomDetailsWidget extends StatelessWidget {
@@ -12,7 +14,8 @@ class CartBottomDetailsWidget extends StatelessWidget {
     Key? key,
     required CartController con,
     required this.selectedTap,
-  }) : _con = con, super(key: key);
+  }) : _con = con,
+       super(key: key);
 
   final CartController _con;
   final int selectedTap;
@@ -22,156 +25,167 @@ class CartBottomDetailsWidget extends StatelessWidget {
     return _con.carts.isEmpty
         ? SizedBox(height: 0)
         : Container(
-      padding: EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        color: AppColors.colorFAFAFA,
-        border: Border.all(color: AppColors.colorF1F1F1),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width - 40,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Text(
-              S.of(context).order_summary,
-              style: TextStyle(
-                fontFamily: 'Nunito',
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-                color: Color(0xFF272727),
-              ),
-            ),
-            SizedBox(height: 4),
-            Text(
-              S.of(context).include_tax,
-              style: TextStyle(
-                fontFamily: 'Nunito',
-                fontSize: 12,
-                fontWeight: FontWeight.w400,
-                color: Color(0xFF9D9FA4),
-              ),
-            ),
-            SizedBox(height: 16),
-
-            // Subtotal
-            Row(
+          padding: EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: AppColors.colorFAFAFA,
+            border: Border.all(color: AppColors.colorF1F1F1),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width - 40,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
               children: <Widget>[
-                Expanded(
-                  child: Text(
-                    S.of(context).subtotal,
-                    style: TextStyle(
-                      fontFamily: 'Nunito',
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400,
-                      color: Color(0xFF9D9FA4),
-                    ),
+                Text(
+                  S.of(context).order_summary,
+                  style: TextStyle(
+                    fontFamily: 'Nunito',
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF272727),
                   ),
                 ),
-                Helper.getPrice(
-                  _con.subTotal,
-                  context,
-                  style: Theme.of(context).textTheme.titleMedium,
-                  zeroPlaceholder: '0',
+                SizedBox(height: 4),
+                Text(
+                  S.of(context).include_tax,
+                  style: TextStyle(
+                    fontFamily: 'Nunito',
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF9D9FA4),
+                  ),
                 ),
-              ],
-            ),
-            SizedBox(height: 5),
+                SizedBox(height: 16),
 
-            // Delivery Fee – يظهر فقط إذا كانت Delivery
-            if (selectedTap == 1)
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      S.of(context).delivery_fee,
+                // Subtotal
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        S.of(context).subtotal,
+                        style: TextStyle(
+                          fontFamily: 'Nunito',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFF9D9FA4),
+                        ),
+                      ),
+                    ),
+                    Helper.getPrice(
+                      _con.subTotal,
+                      context,
+                      style: Theme.of(context).textTheme.titleMedium,
+                      zeroPlaceholder: '0',
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5),
+
+                // Delivery Fee – يظهر فقط إذا كانت Delivery
+                if (selectedTap == 1)
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(
+                          S.of(context).delivery_fee,
+                          style: TextStyle(
+                            fontFamily: 'Nunito',
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: Color(0xFF9D9FA4),
+                          ),
+                        ),
+                      ),
+                      Helper.getPrice(
+                        _con.carts[0].food!.restaurant!.deliveryFee,
+                        context,
+                        style: Theme.of(context).textTheme.titleMedium,
+                        zeroPlaceholder: 'Free',
+                      ),
+                    ],
+                  ),
+
+                // Tax
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        '${S.of(context).tax} (${_con.carts[0].food!.restaurant!.defaultTax}%)',
+                        style: TextStyle(
+                          fontFamily: 'Nunito',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFF9D9FA4),
+                        ),
+                      ),
+                    ),
+                    Helper.getPrice(
+                      _con.taxAmount,
+                      context,
                       style: TextStyle(
                         fontFamily: 'Nunito',
                         fontSize: 12,
                         fontWeight: FontWeight.w400,
-                        color: Color(0xFF9D9FA4),
+                        color: Color(0xFF272727),
                       ),
                     ),
-                  ),
-                  Helper.getPrice(
-                    _con.carts[0].food!.restaurant!.deliveryFee,
-                    context,
-                    style: Theme.of(context).textTheme.titleMedium,
-                    zeroPlaceholder: 'Free',
-                  ),
-                ],
-              ),
-
-            // Tax
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text(
-                    '${S.of(context).tax} (${_con.carts[0].food!.restaurant!.defaultTax}%)',
-                    style: TextStyle(
-                      fontFamily: 'Nunito',
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400,
-                      color: Color(0xFF9D9FA4),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Divider(color: AppColors.colorF1F1F1),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      S.of(context).total,
+                      style: TextStyle(
+                        fontFamily: 'Nunito',
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xFF272727),
+                      ),
                     ),
-                  ),
+                    _buildPriceWidget(context),
+                  ],
                 ),
-                Helper.getPrice(
-                  _con.taxAmount,
-                  context,
-                  style: TextStyle(
-                    fontFamily: 'Nunito',
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
-                    color: Color(0xFF272727),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Divider(color: AppColors.colorF1F1F1),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  S.of(context).total,
-                  style: TextStyle(
-                    fontFamily: 'Nunito',
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
-                    color: Color(0xFF272727),
-                  ),
-                ),
-                _buildPriceWidget(context),
-              ],
-            ),
-            SizedBox(height: 10),
+                SizedBox(height: 10),
 
-            // Checkout Button
-            Stack(
-              fit: StackFit.loose,
-              alignment: AlignmentDirectional.centerEnd,
-              children: <Widget>[
-                SizedBox(
-                  width: MediaQuery.of(context).size.width - 40,
-                  child: CustomMaterialButton(
-                    onPressed: _con.isLoading
-                        ? null
-                        : () => _con.goCheckout(context),
-                  ),
+                // Checkout Button
+                Stack(
+                  fit: StackFit.loose,
+                  alignment: AlignmentDirectional.centerEnd,
+                  children: <Widget>[
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width - 40,
+                      child: CustomMaterialButton(
+                        onPressed: _con.isLoading
+                            ? null
+                            : () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (_) => OrderSuccessWidget(
+                                routeArgument: RouteArgument(
+                                  param: 'Cash on Delivery',
+                                ),
+                              ),
+                            ),
+                          );
+                          print("mElkerm : Order Success");
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: _buildPriceWidget(context),
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: _buildPriceWidget(context),
-                ),
+                SizedBox(height: 10),
               ],
             ),
-            SizedBox(height: 10),
-          ],
-        ),
-      ),
-    );
+          ),
+        );
   }
 
   Widget _buildPriceWidget(BuildContext context) {
