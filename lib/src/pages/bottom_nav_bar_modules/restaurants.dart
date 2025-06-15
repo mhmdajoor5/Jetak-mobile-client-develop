2ede14e7043a08e55694f24cd4e9982b39039624
Mohamed Elkerm Laptop <mohamedelkerm584@gmail.com>
2025-06-15 03:49:41 +0300

    add pagination to rest screen

--- lib/src/pages/bottom_nav_bar_modules/restaurants.dart
+++ lib/src/pages/bottom_nav_bar_modules/restaurants.dart
@@ -1,71 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:mvc_pattern/mvc_pattern.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import '../../../generated/l10n.dart';
-import '../../controllers/home_controller.dart';
-import '../../elements/GridWidget.dart';
 import '../../models/restaurant.dart';
-import 'package:http/http.dart' as http;
+import '../../elements/grid_card_widget.dart';
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class RestaurantsWidget extends StatefulWidget {
   final GlobalKey<ScaffoldState>? parentScaffoldKey;
 
-  RestaurantsWidget({Key? key, this.parentScaffoldKey}) : super(key: key);
+  const RestaurantsWidget({Key? key, this.parentScaffoldKey}) : super(key: key);
 
   @override
-  _RestaurantsWidgetState createState() => _RestaurantsWidgetState();
+  State<RestaurantsWidget> createState() => _RestaurantsWidgetState();
 }
 
-class _RestaurantsWidgetState extends StateMVC<RestaurantsWidget> {
-  late HomeController _con;
-  int currentPage = 1;
-  bool isLoading = false;
-  bool hasMore = true;
-  List<Restaurant> _restaurants = [];
-  final ScrollController _scrollController = ScrollController();
+class _RestaurantsWidgetState extends State<RestaurantsWidget> {
+  static const _pageSize = 10;
 
-  _RestaurantsWidgetState() : super(HomeController()) {
-    _con = controller as HomeController;
-  }
+  final PagingController<int, Restaurant> _pagingController =
+  PagingController(firstPageKey: 1);
 
   @override
   void initState() {
-    super.initState();
-    fetchRestaurants();
-    _scrollController.addListener(() {
-      if (_scrollController.position.pixels >= _scrollController.position.maxScrollExtent - 300 && !isLoading && hasMore) {
-        fetchRestaurants();
-      }
+    _pagingController.addPageRequestListener((pageKey) {
+      _fetchPage(pageKey);
     });
+    super.initState();
   }
 
-  Future<void> fetchRestaurants() async {
-    setState(() => isLoading = true);
-
+  Future<void> _fetchPage(int pageKey) async {
     try {
-      final response = await http.get(Uri.parse('https://carrytechnologies.co/api/restaurants?page=$currentPage'));      if (response.statusCode == 200) {
+      final response = await http.get(Uri.parse(
+        'https://carrytechnologies.co/api/restaurants?page=$pageKey',
+      ));
+
+      if (response.statusCode == 200) {
         final data = json.decode(response.body);
         final List list = data['data']['data'];
+        final int lastPage = data['data']['last_page'];
 
-        if (list.isEmpty) {
-          setState(() => hasMore = false);
+        final newItems = list.map((e) => Restaurant.fromJSON(e)).toList();
+
+        final isLastPage = pageKey >= lastPage;
+        if (isLastPage) {
+          _pagingController.appendLastPage(newItems);
         } else {
-          setState(() {
-            _restaurants.addAll(list.map((e) => Restaurant.fromJSON(e)).toList());
-            currentPage++;
-          });
+          final nextPageKey = pageKey + 1;
+          _pagingController.appendPage(newItems, nextPageKey);
         }
+      } else {
+        _pagingController.error = 'Failed to load data';
       }
-    } catch (e) {
-      print('Error loading restaurants: $e');
+    } catch (error) {
+      _pagingController.error = error;
     }
-
-    setState(() => isLoading = false);
   }
 
   @override
   void dispose() {
-    _scrollController.dispose();
+    _pagingController.dispose();
     super.dispose();
   }
 
@@ -83,25 +78,30 @@ class _RestaurantsWidgetState extends StateMVC<RestaurantsWidget> {
         centerTitle: true,
         title: Text(
           S.of(context).restaurants,
-          style: Theme.of(context).textTheme.headlineSmall?.merge(TextStyle(letterSpacing: 1.3)),
+          style: Theme.of(context)
+              .textTheme
+              .headlineSmall
+              ?.merge(TextStyle(letterSpacing: 1.3)),
         ),
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Column(
-          children: [
-            Expanded(
-              child: GridWidget(
-                restaurantsList: _restaurants,
-                heroTag: 'home_restaurants',
+        child: RefreshIndicator(
+          onRefresh: () => Future.sync(() => _pagingController.refresh()),
+          child: PagedMasonryGridView.count(
+            pagingController: _pagingController,
+            crossAxisCount: MediaQuery.of(context).orientation == Orientation.portrait ? 2 : 4,
+            mainAxisSpacing: 15,
+            crossAxisSpacing: 15,
+            builderDelegate: PagedChildBuilderDelegate<Restaurant>(
+              itemBuilder: (context, item, index) => GridCardWidget(
+                restaurant: item,
+              ),
+              noItemsFoundIndicatorBuilder: (_) => Center(
+                child: Text('S.of(context).no_restaurants_found'),
               ),
             ),
-            if (isLoading)
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: CircularProgressIndicator(),
-              ),
-          ],
+          ),
         ),
       ),
     );
